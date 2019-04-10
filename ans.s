8d96eeae513cc4182866f3e540c29a35d8a737e5
fernal73 <linus.fernandes@gmail.com>
2019-04-11 05:19:02 +0530

    Added assembly code with entry begin and modified build file to set entry function

---
+++ ans.s
@@ -0,0 +1,5 @@
+.global begin
+ begin:
+ mov x0, #42
+ mov x8, #93
+ svc #0
---
+++ testentry.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main();
+int test(void)
+{
+printf("Hello\n");
+main();
+return 1;
+}
+
+
+int main()
+{
+return printf("OK\n");
+}
